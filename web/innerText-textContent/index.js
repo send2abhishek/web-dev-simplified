9b04c8e194584cf3951034af2d684c2ecc5dff2c
Abhishek Kumar <abhishek160.kumar@ril.com>
2022-01-02 15:02:34 +0530

    feat: Diff between innerText and textContent

---
+++ web/innerText-textContent/index.js
@@ -0,0 +1,8 @@
+const head = document.querySelector("h1");
+console.log(head.innerText);
+
+const para = document.querySelector("p");
+console.log(para.textContent);
+
+// innerText return the text with removing the white space around
+// textContent return the text with space if present
